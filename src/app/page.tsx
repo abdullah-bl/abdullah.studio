60249608ecfa64391eea0d972804e79d92c53b44
abdullah-bl <abdullah-dev@hotmail.com>
2024-09-20 19:51:42 +0300

    moved to Next js

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Abdullah Bl</h1>
+        <p className="text-lg">Portfolio</p>
+        <a href="mailto:mail@abdulla.studio">Contact me</a>
+      </div>
+    </main>
+  )
+}
