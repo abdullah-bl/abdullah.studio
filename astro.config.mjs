a8f3a22c857a9111e6c33930a88a1407d85bce0a
Abdullah Bl <abdullah-dev@hotmail.com>
2025-07-04 22:42:44 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
         // Reference the separate Vite config for build optimizations
         configFile: './vite.config.js',
     },
+
     integrations: [react(), mdx(), partytown()],
 });
